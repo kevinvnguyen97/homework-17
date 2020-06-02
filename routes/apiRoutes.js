7c1ed48db7626dfdf0d2ac0eba3405702120a406
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-06-02 01:29:11 -0500

    Workouts now save. Time does not display properly.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-
 var db = require("../models");
 const router = require("express").Router();
 
@@ -13,15 +12,30 @@ router.get("/api/workouts", function (req, res) {
 });
 
 router.post("/api/workouts", function (req, res) {
+    console.log("Req body: ");
+    console.log(req.body);
     db.Workout.create(req.body)
         .then(dbWorkout => { res.json(dbWorkout) })
         .catch(err => { res.json(err) });
+});
+
+router.put("/api/workouts/:id", function(req, res) {
+    console.log(req.params.id);
+    db.Workout.findByIdAndUpdate({
+        _id: req.params.id
+    }, {
+        $push: {
+            exercises: req.body
+        }
+    })
+    .then(dbWorkout => {res.json(dbWorkout);})
+    .catch(err => {res.json(err);})
 });
 
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
         .then(dbRange => { res.json(dbRange); })
         .catch(err => console.log(err));
-})
+});
 
 module.exports = router;
